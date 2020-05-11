8e19c718005ba177688201d215c6980bf024682c
Anjan <saianjan.margani@gmail.com>
2020-05-11 22:30:29 +0530

    Updates

---
+++ precache-manifest.d311c3d4b479cdeffd0b1ae3089c4e92.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534b662258bae2828948dec3e70cfd8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0962e30a3118cf5aaaaf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b674eae1eedc175bb5e",
+    "url": "/static/js/2.72c324ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72c324ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0962e30a3118cf5aaaaf",
+    "url": "/static/js/main.930e45ea.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "09879d6a1d5a40f51ad24c893a104d4d",
+    "url": "/static/media/U1.09879d6a.png"
+  },
+  {
+    "revision": "332e40a0e47a82d4c497f2c83e547ea4",
+    "url": "/static/media/U1.332e40a0.jpg"
+  },
+  {
+    "revision": "7a706a529037bbaa0f4ba47485551ca3",
+    "url": "/static/media/U1.7a706a52.png"
+  },
+  {
+    "revision": "7c5714e3a85758f8112fef56a1e55883",
+    "url": "/static/media/U1.7c5714e3.png"
+  },
+  {
+    "revision": "cff5013501289c9b6a820b088032480c",
+    "url": "/static/media/U2.cff50135.png"
+  },
+  {
+    "revision": "66ec0a6353201a75e9d44ab2d90747e7",
+    "url": "/static/media/U3.66ec0a63.png"
+  },
+  {
+    "revision": "3e92f8824c267ac4d7b17675d0ddbf1e",
+    "url": "/static/media/U4.3e92f882.png"
+  },
+  {
+    "revision": "0bdec49b40127710f000a0d6b33d508b",
+    "url": "/static/media/U5.0bdec49b.png"
+  },
+  {
+    "revision": "f8fd6d02b16b4233a0dc61b0352ff053",
+    "url": "/static/media/U6.f8fd6d02.png"
+  },
+  {
+    "revision": "fb9783fde02e48ad590672d0c9019509",
+    "url": "/static/media/atomic.fb9783fd.jpg"
+  },
+  {
+    "revision": "3fb5ebd3c4b7f8f6334677b398fc350e",
+    "url": "/static/media/byk.3fb5ebd3.png"
+  },
+  {
+    "revision": "6e61f5b0a145eee2418077684c52e49e",
+    "url": "/static/media/close_up.6e61f5b0.png"
+  },
+  {
+    "revision": "927ab4da5e09129e337fb8e403c29311",
+    "url": "/static/media/game.927ab4da.jpeg"
+  },
+  {
+    "revision": "6e07b9edb5eb46a1669543b78cbf53ee",
+    "url": "/static/media/map.6e07b9ed.jpg"
+  },
+  {
+    "revision": "6656db610546ba3d867773cf1d473103",
+    "url": "/static/media/men.6656db61.png"
+  },
+  {
+    "revision": "3eb1b32f2996920e0d4e25d613211412",
+    "url": "/static/media/p3.3eb1b32f.png"
+  },
+  {
+    "revision": "9cb3c7741cbd021ae4c08d7620b4859a",
+    "url": "/static/media/p9.9cb3c774.png"
+  },
+  {
+    "revision": "b492db2c9b554f564164cebba8832356",
+    "url": "/static/media/pedl_cyclo.b492db2c.jpg"
+  },
+  {
+    "revision": "ba50226108a5ca5b452f2f5f64263f61",
+    "url": "/static/media/playds.ba502261.jpg"
+  },
+  {
+    "revision": "a15d3733fb5ed6842e82b860d9bca096",
+    "url": "/static/media/process.a15d3733.png"
+  },
+  {
+    "revision": "bcaabf6eacb18eb87340a0836728ed2d",
+    "url": "/static/media/resume.bcaabf6e.pdf"
+  },
+  {
+    "revision": "fec67575cc7dcc72ac053b98482232f9",
+    "url": "/static/media/srcone.fec67575.jpg"
+  },
+  {
+    "revision": "51913a328349bcf6699c94908d1f85cc",
+    "url": "/static/media/srcthree.51913a32.jpg"
+  },
+  {
+    "revision": "e4ac5fe7041567a7a973f813f2396cb3",
+    "url": "/static/media/srctwo.e4ac5fe7.jpg"
+  },
+  {
+    "revision": "3bd63afa23f3ed09050b2116964ba384",
+    "url": "/static/media/ui1.3bd63afa.png"
+  },
+  {
+    "revision": "7fd01830d23027cfec26e6706b924236",
+    "url": "/static/media/ui2.7fd01830.png"
+  },
+  {
+    "revision": "f5d3c421fb2fa6153e0603b75f0dfb01",
+    "url": "/static/media/ui3.f5d3c421.png"
+  },
+  {
+    "revision": "c456cf963339e00d43f751eec651a4b2",
+    "url": "/static/media/video.c456cf96.mp4"
+  }
+]);
